996f97bb176b189c60f8751ff07689050248cde2
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 21:11:41 +0630

    Created functinality to check permission of login user

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -16,6 +16,15 @@ const authenticateUser = (req, res, next) => {
     }
 }
 
+const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role))
+            throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+        next()
+    }
+}
+
 module.exports = {
     authenticateUser,
+    authorizePermissions
 }
