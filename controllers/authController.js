202888b907c6d80cf1e11fbd9f8369172ff303f5
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 20:16:28 +0630

    Added login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,24 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login')
+    const { email, password } = req.body
+
+    if (!email || !password)
+        throw new CustomError.BadRequestError('Please provide email and password')
+
+    const user = await User.findOne({ email })
+    if (!user)
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect)
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({ res, user: tokenUser })
+
+    res.status(StatusCodes.OK).json({ user: tokenUser })
+
 }
 
 const logout = async (req, res) => {
