e9a9c61ba02426ec56ec5c117fc6798b4c6603ae
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 22:05:17 +0630

    Added functionality for user to change new password

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,21 @@ const updateUser = async (req, res) => {
 }
 
 const updateUserPassword = async (req, res) => {
-    res.send('update user password')
+    const { oldPassword, newPassword } = req.body
+    if (!oldPassword || !newPassword)
+        throw new CustomError.BadRequestError('Please provide your current password and new one')
+
+    const user = await User.findOne({ _id: req.user.userId })
+
+    const isPasswordCorrect = await user.comparePassword(oldPassword)
+    if (!isPasswordCorrect)
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+
+    user.password = newPassword
+
+    await user.save()
+
+    res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated' })
 }
 
 module.exports = {
