80904b10c85fc6af2c1e3299835bb0de970e7eff
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 16:08:12 +0630

    Added a feature to response back with cookie in register route

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
-            console.log(`Server is listening: http://localhost:${port}`)
+            console.log(`Server is listening on port ${port}...`)
         })
     } catch (error) {
         console.log(error)
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const { createTokenUser, attachCookiesToResponse } = require('../utils')
 
 const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -14,7 +15,10 @@ const register = async (req, res) => {
 
     const user = await User.create({ name, email, password, role })
 
-    res.status(StatusCodes.CREATED).json({ user })
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({ res, user: tokenUser })
+
+    res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 
 const login = async (req, res) => {
