bfc6b972ed4d23267492dc630c85eb92998834b5
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 21:07:43 +0630

    Created custom unauthorized error handler

--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,12 @@ const CustomAPIError = require('./custom-api')
 const BadRequestError = require('./bad-request')
 const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
+const UnauthorizedError = require('./unauthorized')
 
 module.exports = {
     CustomAPIError,
     BadRequestError,
     UnauthenticatedError,
-    NotFoundError
+    NotFoundError,
+    UnauthorizedError
 }
