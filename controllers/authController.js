9dd13048850751abe1440002fd1bc73951fe407c
Hein Swan Htet <hshscientist@gmail.com>
2024-03-26 20:20:57 +0630

    Added logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,7 +43,12 @@ const login = async (req, res) => {
 }
 
 const logout = async (req, res) => {
-    res.send('logout')
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    })
+
+    res.status(StatusCodes.OK).json({ msg: 'Successfully logged out!' })
 }
 
 module.exports = {
