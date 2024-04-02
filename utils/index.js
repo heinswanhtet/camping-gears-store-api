469c6d68a52086197512817f5c8586218e563415
Hein Swan Htet <hshscientist@gmail.com>
2024-04-02 11:12:45 +0630

    Created utility function to send email for reset password link

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,7 @@ const createTokenUser = require('./createTokenUser')
 const checkPermissions = require('./checkPermissions')
 const sendInvoiceEmail = require('./sendInvoiceEmail')
 const sendVerificationEmail = require('./sendVerificationEmail')
+const sendResetPasswordEmail = require('./sendResetPasswordEmail')
 
 module.exports = {
     createJWT,
@@ -11,5 +12,5 @@ module.exports = {
     createTokenUser,
     checkPermissions,
     sendInvoiceEmail,
-    sendVerificationEmail
+    sendResetPasswordEmail
 }
